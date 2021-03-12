dcb0a2ab7de429f6b947c411a11416ca5772a17e
Maranda <donal163@umn.edu>
2021-03-12 10:12:17 -0600

    testing + some bug fixes

--- Project1/testing/TestBallot.java
+++ Project1/testing/TestBallot.java
@@ -3,10 +3,46 @@ import static org.junit.Assert.*;
 
 public class TestBallot {
 
+    private Ballot ballot1;
+    private Ballot ballot2;
+    private Ballot ballot3;
+    private Ballot ballot4 = null;
+    private Candidate candidate;
+
     @Test
-    public void testAdd() {
-        String str = "Junit is working fine";
-        assertEquals("Junit is working fine",str);
+    public void testAddBallot() {
+        //dont know if i should make an example ballot to test this?
+        ballot1.setID(001);
+        ballot2.setID(002);
+        ballot3.setID(003);
+
+        assertEquals(ballot1.getID(), 001);
+        assertEquals(ballot2.getID(), 002);
+        assertEquals(ballot3.getID(), 003);
+        assertEquals(ballot4.getID(), null);
+
+        assertTrue(ballot1.getID() != 0);
+        assertTrue(ballot2.getID() != 0);
+        assertTrue(ballot3.getID() != 0);
+
+        candidate.addBallot(ballot1);
+        candidate.addBallot(ballot2);
+
+        assertEquals(candidate.getcBallots().get(0), ballot1);
+        assertEquals(candidate.getcBallots().get(1), ballot2);
+        assertEquals(candidate.getcNumBallots(), 2);
+
+        candidate.addBallot(ballot4);
+        assertEquals(candidate.getcBallots().get(0), ballot1);
+        assertEquals(candidate.getcBallots().get(1), ballot2);
+        assertEquals(candidate.getcNumBallots(), 2);
+
+        candidate.addBallot(ballot3);
+        assertEquals(candidate.getcBallots().get(0), ballot1);
+        assertEquals(candidate.getcBallots().get(1), ballot2);
+        assertEquals(candidate.getcBallots().get(2), ballot3);
+        assertEquals(candidate.getcNumBallots(), 3);
+
     }
 
 }
--- Project1/testing/TestParty.java
+++ Project1/testing/TestParty.java
@@ -1,33 +1,21 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class PartyTest {
-
-    private Candidate candidate1;
-    private Candidate candidate2;
-    private Candidate candidate3;
-    private Candidate candidate4;
-    private Party bestParty;
+public class TestParty {
 
     @Test
     public void testAddCandidate() {
+        Candidate candidate1;
+        Candidate candidate2;
+        Party bestParty;
+
         bestParty = new Party("P");
         candidate1 = new Candidate("name", "P");
         candidate2 = new Candidate("name", "P");
-        candidate3 = null;
-        candidate4 = new Candidate("name", "notP");
 
-        // General Case
         bestParty.addCandidate(candidate1);
         bestParty.addCandidate(candidate2);
-        assertEquals(bestParty.getCandidates().length(), 2); // add two regular candidate objects
 
-        // Insert Null Case
-        bestParty.addCandidate(candidate3);
-        assertEquals(bestParty.getCandidates().length(), 2); // should NOT have added the null candidate
-
-        // Insert Candidate without a matching party
-        
+        assertEquals(bestParty.getCandidates().size(), 2);
     }
-
 }
