83d91a2b47e356cfcbce7a3427f65e5591490068
Eileen Campbell <camp0870@umn.edu>
2021-03-11 22:47:22 -0600

    More Tests

--- Project1/testing/TestVotingSystem.java
+++ Project1/testing/TestVotingSystem.java
@@ -2,6 +2,24 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestVotingSystem {
+    private VotingSystem votingSys;
 
+    @Test
+    Public void testCoinToss() {
+        votingSys = new VotingSystem();
+
+        int result = votingSys.coinToss(-5);
+        assertEquals(result, -1);
+        result = 10;
+
+        result = votingSys.coinToss(0);
+        assertEquals(result, -1);
+
+        for (int i = 0; i < 26; i++) {
+            result = votingSys.coinToss(13);
+            assertTrue("Random Number too High", result < 13);
+            assertTrue("Random Number too Low", result >= 0);
+        }
+    }
 
 }
